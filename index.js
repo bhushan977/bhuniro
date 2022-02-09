4b4f50833712dca988caae2a22663e8232f1aab1
Bhushan Niroula <quitboy568@gmail.com>
2022-02-09 22:11:25 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//   1 ways to print in javascript
+// console.log("Bhuniro Does Namesta");
+// alert("we");
+// document.write("this is")
+//    2javascript consloe API
+// console.log("Bhuniro Does Namesta", 10 + 2, "arko ");
+// console.warn("naher")
+// console.error("error")
+
+// 3. JavaScript Variables
+// what is variables? containers to store data values
+var number1 = 34;
+var number2 = 56;
+console.log(number1 + number2);
+ 
+
+
